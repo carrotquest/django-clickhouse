da755da09d9b17576840319c25408f98351650db
M1ha <work_shvein_mihail@mail.ru>
2018-11-22 11:18:53 +0500

    Added wrappers for Infi querysets

--- src/django_clickhouse/query.py
+++ src/django_clickhouse/query.py
@@ -1,3 +1,14 @@
+from infi.clickhouse_orm.query import QuerySet as InfiQuerySet, AggregateQuerySet as InfiAggregateQuerySet
+
+
+class QuerySet(InfiQuerySet):
+    pass
+
+
+class AggregateQuerySet(InfiAggregateQuerySet):
+    pass
+
+
 # class ClickHouseModel(InfiModel):
 #
 #     @classmethod
