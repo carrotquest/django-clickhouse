9d8bdb74b9d6c87bb228ed3d247f34b2e02a75a1
M1ha <work_shvein_mihail@mail.ru>
2019-10-15 18:13:42 +0500

    Bugfix for old django, expecting manager to be instance of models.Manager

--- src/django_clickhouse/models.py
+++ src/django_clickhouse/models.py
@@ -7,7 +7,7 @@ from typing import Optional, Any, Type, Set
 
 import six
 from django.db import transaction
-from django.db.models.manager import BaseManager
+from django.db.models.manager import BaseManager, Manager
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.db.models import QuerySet as DjangoQuerySet, Model as DjangoModel
@@ -116,7 +116,7 @@ qs_bases.append(DjangoQuerySet)
 ClickHouseSyncQuerySet = type('ClickHouseSyncModelQuerySet', tuple(qs_bases), {})
 
 
-class ClickHouseSyncManager(BaseManager.from_queryset(ClickHouseSyncQuerySet), models.Manager):
+class ClickHouseSyncManager(BaseManager.from_queryset(ClickHouseSyncQuerySet), Manager):
     pass
 
 
