9ff1da8b1551766ef9b49b9a9098c7ea8ad52d6d
amirhgh <amirhgholamian78@gmail.com>
2021-12-31 22:07:28 +0330

    update test migration

--- tests/clickhouse_migrations/0001_initial.py
+++ tests/clickhouse_migrations/0001_initial.py
@@ -1,10 +1,10 @@
 
-from django_clickhouse import migrations
+from django_clickhouse import migrations, migration_operators
 from tests.clickhouse_models import ClickHouseTestModel, ClickHouseCollapseTestModel
 
 
 class Migration(migrations.Migration):
     operations = [
-        migrations.CreateTable(ClickHouseTestModel),
-        migrations.CreateTable(ClickHouseCollapseTestModel)
+        migration_operators.CreateTable(ClickHouseTestModel),
+        migration_operators.CreateTable(ClickHouseCollapseTestModel)
     ]
